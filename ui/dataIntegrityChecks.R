8ce20ad269758fca238b26a9d49b8c1f7c2988e7
Stephanie Reinders <reinders.stephanie@gmail.com>
2022-08-29 11:31:52 -0500

    delete old headings

--- ui/dataIntegrityChecks.R
+++ ui/dataIntegrityChecks.R
@@ -5,22 +5,10 @@ tabPanel("Data Integrity Checks",
            sidebarPanel(width = 4,
                         h2("Data Integrity Checks"),
                         br(),
-                        h4("Current document:"),
-                        fluidRow(column(width=11, offset=1, textOutput("image_name"))),
-                        fluidRow(column(width=11, offset=1, textOutput("dimensions"))),
+                        h4("Select Folder:")
                        ),
            mainPanel(width = 8,
-                     span(textOutput("error"), style="color:red"),
-                     br(),
-                     tabsetPanel(id = "plotset",
-                                 tabPanel("Current Document",
-                                          br(),
-                                 ),
-                                 tabPanel("Apply Mask",
-                                          br(),
-                                 )
-                     ),
-                     
+                     span(textOutput("error"), style="color:red")
            )
          ),
 )
